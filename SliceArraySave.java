41f27661b441cbf7264495c5f514d988ebf1180e
alpha0212 <qkrcjfgml321@naver.com>
2023-01-12 01:24:13 +0900

    java: SliceArraySave

---
+++ SliceArraySave.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String[] solution(String my_str, int n) {
+        String[] answer = { my_str };
+        for (int i = 0; i < answer.length; i++) {
+        }
+        return answer;
+    }
+}
