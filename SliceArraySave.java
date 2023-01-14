8b4d97e8b997831d7622b61f47d6aa1e1eef3e91
alpha0212 <qkrcjfgml321@naver.com>
2023-01-14 23:43:08 +0900

    Java Level0: SliceArraySave

--- SliceArraySave.java
+++ SliceArraySave.java
@@ -1,8 +1,19 @@
+import java.util.*;
 class Solution {
     public String[] solution(String my_str, int n) {
-        String[] answer = { my_str };
-        for (int i = 0; i < answer.length; i++) {
+		Double cnt = (double) Math.ceil((double) my_str.length() / n) ;
+        String[] answer = new String[cnt.intValue()];
+        int index = 0;
+        for(int i = 0; i < cnt.intValue(); i++) {
+            if(my_str.length() % n == 0) {
+                answer[i] = my_str.substring(index, index+n);
+            } else if (i == cnt - 1) {
+                answer[i] = my_str.substring(index);
+            } else {
+                answer[i] = my_str.substring(index, index+n);
+            }
         }
-        return answer;
+    index+=n;
+    return answer;
     }
 }
